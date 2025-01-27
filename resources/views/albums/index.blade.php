a2cb153736ecf133dcae88da7ea5d28310761368
Lukyano <lukas.kert@email.cz>
2025-01-27 18:01:38 +0100

    adding images

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h3>Albums</h3>
+<h1>PhotoGallery</h1>
+<div class="imageGallery">
+    <span><img src="/images/Image-1.jpg"></span>
+    <span><img src="/images/Image-2.jpg"></span>
+    <span><img src="/images/Image-3.jpeg"></span>
+</div>
+<script>
+    window.ws = new WebSlides();
+</script>
 @endsection
